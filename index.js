653bce0802a95d45697e357631c5140fc308fa39
fmquaglia <matias.quaglia@gmail.com>
2021-07-02 11:00:13 -0300

    WIP

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const {connect, StringCodec} = require("nats");
 
 
 (async () => {
-  const natsConnection = await connect({servers: process.env.NATS_SERVER});
+  const name = `connection ${Math.ceil(Math.random()*100+1)}`;
+  const natsConnection = await connect({
+    servers: process.env.NATS_SERVER,
+    name
+  });
   const stringCoded = StringCodec();
   const subscription = natsConnection.subscribe(process.env.NATS_SUBJECT);
   console.log('subscription started');
-  console.log(subscription);
+
   for await (const message of subscription) {
     const decodedMessage = stringCoded.decode(message.data);
-    console.log(`[${subscription.getProcessed()}]: ${decodedMessage}`);
-
-    if (decodedMessage === `GOOD BYE ${subscription.sid}`) {
+    console.log(`[${subscription.protocol.options.name}][${subscription.getProcessed()}]: ${decodedMessage}`);
+    if (decodedMessage === `GOOD BYE ${name}`) {
       await natsConnection.drain();
     }
   }
+
   console.log("subscription closed");
 })();
