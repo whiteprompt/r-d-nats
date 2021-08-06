4ee6a49498a3114c47bdc6801c8b014ebb0cfcd5
fmquaglia <matias.quaglia@gmail.com>
2021-08-06 12:38:19 -0300

    WIP

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
-const {connect, StringCodec} = require("nats");
+const {connect, StringCodec} = require('nats');
+const Web3 = require('web3');
+const {abi} = require('./Feature.json');
+// const websocketProvider = new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/45261a9a09fa4ce683bb25ae00b8c079');
+const websocketProvider = new Web3.providers.WebsocketProvider('ws://127.0.0.1:9545/');
+const web3 = new Web3(websocketProvider);
 
+// const contractInstance = new web3.eth.Contract(abi, '0x608fa86C629283Bc3500d8BaC1842Fb315862bC9');
+const contractAddress = '0x8a080f066f60072aEAFDDC310EeDD3d09980b315';
+const contractInstance = new web3.eth.Contract(abi, contractAddress);
+
+async function getEvents(fromBlock = 'latest', toBlock = 'latest', opts = {}) {
+  const options = {fromBlock, toBlock, ...opts};
+  return await contractInstance.getPastEvents(
+    'Burned',
+    options
+  );
+}
+
+const subscription = web3.eth.subscribe(
+  'logs',
+  {
+    address: contractAddress,
+    from: 0,
+  },
+  (error, result) => {
+    if (error) return;
+    // do something with the data
+    console.log(result);
+  }
+);
+
+async function poll(fn, time) {
+  await fn();
+  setTimeout(fn, time);
+}
+
+async function watch(fn = async () => {}, millis=10000) {
+  poll(fn, millis);
+}
 
 (async () => {
-  const name = `connection ${Math.ceil(Math.random()*100+1)}`;
+  // await watch(getEvents);
+  const name = `connection ${Math.ceil(Math.random() * 100 + 1)}`;
   const natsConnection = await connect({
     servers: process.env.NATS_SERVER,
     name
