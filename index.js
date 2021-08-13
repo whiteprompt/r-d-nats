89fbe8767e1a52ecac9e101baf92b6bcde46ce2a
fmquaglia <matias.quaglia@gmail.com>
2021-08-13 13:16:30 -0300

    adds functionality to put data on chain

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
 const {connect, StringCodec} = require('nats');
-const Web3 = require('web3');
-const {abi} = require('./Feature.json');
-// const websocketProvider = new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/45261a9a09fa4ce683bb25ae00b8c079');
-const websocketProvider = new Web3.providers.WebsocketProvider('ws://127.0.0.1:9545/');
-const web3 = new Web3(websocketProvider);
+const PackageContract = require('./blockhain.js');
 
-// const contractInstance = new web3.eth.Contract(abi, '0x608fa86C629283Bc3500d8BaC1842Fb315862bC9');
-const contractAddress = '0x8a080f066f60072aEAFDDC310EeDD3d09980b315';
-const contractInstance = new web3.eth.Contract(abi, contractAddress);
-
-async function getEvents(fromBlock = 'latest', toBlock = 'latest', opts = {}) {
-  const options = {fromBlock, toBlock, ...opts};
-  return await contractInstance.getPastEvents(
-    'Burned',
-    options
-  );
-}
-
-const subscription = web3.eth.subscribe(
-  'logs',
+const package = new PackageContract(
+  './blockchain/build/contracts/Package.json',
+  '0x8a080f066f60072aEAFDDC310EeDD3d09980b315',
+  'http://127.0.0.1:9545/',
   {
-    address: contractAddress,
-    from: 0,
-  },
-  (error, result) => {
-    if (error) return;
-    // do something with the data
-    console.log(result);
+    mnemonic: 'ecology sail runway hidden clump rule range energy awesome nation execute light',
+    minterAccount: '0x5746df578f3b10b83b1e5fbbd533bcff739a7f22'
   }
 );
 
-async function poll(fn, time) {
-  await fn();
-  setTimeout(fn, time);
-}
-
-async function watch(fn = async () => {}, millis=10000) {
-  poll(fn, millis);
-}
-
 (async () => {
-  // await watch(getEvents);
-  const name = `connection ${Math.ceil(Math.random() * 100 + 1)}`;
+  const connectionName = `connection ${Math.ceil(Math.random() * 100 + 1)}`;
   const natsConnection = await connect({
     servers: process.env.NATS_SERVER,
-    name
+    name: connectionName
   });
   const stringCoded = StringCodec();
   const subscription = natsConnection.subscribe(process.env.NATS_SUBJECT);
-  console.log('subscription started');
+  const closeMessage = `GOOD BYE ${connectionName}`;
+  console.log(`close message is: "${closeMessage}" - length: ${closeMessage.length}`);
 
   for await (const message of subscription) {
     const decodedMessage = stringCoded.decode(message.data);
-    console.log(`[${subscription.protocol.options.name}][${subscription.getProcessed()}]: ${decodedMessage}`);
-    if (decodedMessage === `GOOD BYE ${name}`) {
+    if (decodedMessage === `GOOD BYE ${connectionName}`) {
+      console.log('draining connection...');
       await natsConnection.drain();
+    } else if (decodedMessage.startsWith('MINT')) {
+      console.log('minting token...');
+      package.safeMint(decodedMessage).then((data) => {
+        if (data.status === true) {
+          console.log('minted');
+        }
+        console.log(JSON.stringify({data}, null, 2));
+      })
+    } else if (decodedMessage.startsWith('UPDATE')) {
+      console.log('updating token with', decodedMessage);
+      const attributes = decodedMessage.split('UPDATE ')[1].split(',');
+      console.log(attributes);
+      package.addUpdateTo(parseInt(attributes[0]), parseInt(attributes[1])).then((data) => {
+        if (data.status === true) {
+          console.log('updated');
+        }
+        console.log(JSON.stringify({data}, null, 2));
+      })
+    } else {
+      console.log(`[${subscription.protocol.options.name}][${subscription.getProcessed()}]: ${decodedMessage}`);
     }
   }
 
-  console.log("subscription closed");
+  console.log('connection closed');
 })();
